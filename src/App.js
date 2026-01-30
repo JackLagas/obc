62d2aad601037fddd1d123cde11a18bfbb97abd7
Jack Lagas <jacklagasw@gmail.com>
2026-01-30 10:41:50 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     return (
         <div className="App">
 
-            <BrowserRouter>
+            <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
                 <Navbar />
                 <DevBranch />
                 <Routes>
